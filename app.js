59f76fcbcc8983a012f8a12321fa1cd79373b550
sahil khandelwal <khandelwalsahil364@gmail.com>
2025-04-14 02:23:15 +0530

    all the crud operations done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const methodOveride = require("method-override");
 const port = 8080;
 const Listing = require("./models/listing.js");
 const path = require("path");
@@ -22,6 +23,7 @@ main()
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOveride("_method"));
 
 app.get("/", (req, res) => {
   res.send("Hi i am root");
@@ -59,6 +61,43 @@ app.listen(port, () => {
 app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
   res.render("./listings/index.ejs", { allListings });
+});
+
+// create: new and create route
+//new route
+app.get("/listing/new", (req, res) => {
+  res.render("./listings/new.ejs");
+});
+
+// create route
+app.post("/listings", async (req, res) => {
+  //   const { title, description, image, price, location, country } = req.body;
+
+  // or this can be written as:
+  let newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+// update:update and edit route
+// edit
+app.get("/listings/:id/edit", async (req, res) => {
+  const id = req.params.id;
+  const listing = await Listing.findById(id);
+  res.render("./listings/edit.ejs", { listing });
+});
+//update
+app.put("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing }); //this is known as destructuring
+  res.redirect(`/listings/${id}`); //this will redirect it to the show route otherwise dhunda muskil h ki kisme update kia h
+});
+
+//delete route
+app.delete("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
 });
 
 //show route (read operation)
